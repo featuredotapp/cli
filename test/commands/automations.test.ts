2479a0830674e02a1f29048836fdb88f5f4c0d6d
John Kane <john@kanej.me>
2020-11-17 12:58:33 +0000

    test(automations): add 'add' tests for forward/send

--- test/commands/automations.test.ts
+++ test/commands/automations.test.ts
@@ -1,26 +1,127 @@
+/* eslint-disable max-nested-callbacks */
 import { expect, test } from '@oclif/test'
 
 describe('Automations', () => {
-  test
-    .stdout()
-    .nock('http://localhost:7000', (api) =>
-      api.get('/v1/automations').reply(200, { list: [] }),
-    )
-    .command(['automations', 'list'])
-    .exit(0)
-    .it('gives message if no automations', (ctx) => {
-      expect(ctx.stdout).to.contain(
-        "you don't have an automation currently, create one with: mailscript automations add",
+  describe('list', () => {
+    test
+      .stdout()
+      .nock('http://localhost:7000', (api) =>
+        api.get('/v1/automations').reply(200, { list: [] }),
       )
+      .command(['automations', 'list'])
+      .exit(0)
+      .it('gives message if no automations', (ctx) => {
+        expect(ctx.stdout).to.contain(
+          "you don't have an automation currently, create one with: mailscript automations add",
+        )
+      })
+
+    test
+      .stdout()
+      .nock('http://localhost:7000', (api) =>
+        api
+          .get('/v1/automations')
+          .reply(200, { list: [{ id: 'dR862asdfgh' }] }),
+      )
+      .command(['automations', 'list'])
+      .it('lists automations by id', (ctx) => {
+        expect(ctx.stdout).to.contain('dR862asdfgh')
+      })
+  })
+
+  describe('add', () => {
+    let postBody: any
+
+    beforeEach(() => {
+      postBody = {}
     })
 
-  test
-    .stdout()
-    .nock('http://localhost:7000', (api) =>
-      api.get('/v1/automations').reply(200, { list: [{ id: 'dR862asdfgh' }] }),
-    )
-    .command(['automations', 'list'])
-    .it('lists automations by id', (ctx) => {
-      expect(ctx.stdout).to.contain('dR862asdfgh')
+    const nockAdd = (api: any) => {
+      api
+        .persist()
+        .get('/v1/accessories')
+        .query({ name: 'test@mailscript.io' })
+        .reply(200, {
+          id: 'access-01-xxx-yyy-zzz',
+          name: 'test@mailscript.io',
+        })
+
+      api.persist().get('/v1/accessories/test@mailscript.io').reply(200, {
+        id: 'access-01-xxx-yyy-zzz',
+        name: 'test@mailscript.io',
+      })
+
+      return api
+        .post('/v1/automations', (body: any) => {
+          postBody = body
+          return true
+        })
+        .reply(201, { id: 'auto-xxx-yyy-zzz' })
+    }
+
+    describe('forward', () => {
+      test
+        .stdout()
+        .nock('http://localhost:7000', nockAdd)
+        .command([
+          'automations',
+          'add',
+          '--trigger',
+          'test@mailscript.io',
+          '--action',
+          'test@mailscript.io',
+          '--forward',
+          'another@example.com',
+        ])
+        .it('add forward automation', (ctx) => {
+          expect(ctx.stdout).to.contain('auto-xxx-yyy-zzz')
+          expect(postBody.actions[0].config).to.eql({
+            type: 'forward',
+            forward: 'another@example.com',
+          })
+        })
+
+      test
+        .stdout()
+        .nock('http://localhost:7000', nockAdd)
+        .command([
+          'automations',
+          'add',
+          '--trigger',
+          'test@mailscript.io',
+          '--forward',
+          'another@example.com',
+        ])
+        .it('defaults action to trigger if none provided', (ctx) => {
+          expect(ctx.stdout).to.contain('auto-xxx-yyy-zzz')
+        })
     })
+
+    describe('send', () => {
+      test
+        .stdout()
+        .nock('http://localhost:7000', nockAdd)
+        .command([
+          'automations',
+          'add',
+          '--trigger',
+          'test@mailscript.io',
+          '--send',
+          'another@example.com',
+          '--subject',
+          'subject',
+          '--text',
+          'text',
+        ])
+        .it('add send automation', (ctx) => {
+          expect(ctx.stdout).to.contain('auto-xxx-yyy-zzz')
+          expect(postBody.actions[0].config).to.eql({
+            subject: 'subject',
+            text: 'text',
+            to: 'another@example.com',
+            type: 'send',
+          })
+        })
+    })
+  })
 })
