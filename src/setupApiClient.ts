8eb3e66d942cf84947d537d0c34e9acff7d98a3c
John Kane <john@kanej.me>
2020-11-21 16:51:25 +0000

    test: use fake .mailscript config file in tests

--- src/setupApiClient.ts
+++ src/setupApiClient.ts
@@ -7,9 +7,13 @@ import * as api from './api'
 
 const readFile = promisify(readFileRaw)
 
+const {
+  MAILSCRIPT_CONFIG_PATH = path.join(os.homedir(), '.mailscript'),
+} = process.env
+
 async function setupApiClient(): Promise<typeof api> {
   // api.defaults.baseUrl = 'https://example.com/api'
-  const config = await readFile(path.join(os.homedir(), '.mailscript'))
+  const config = await readFile(MAILSCRIPT_CONFIG_PATH)
   const { apiKey } = JSON.parse(config.toString())
 
   api.defaults.headers = {
