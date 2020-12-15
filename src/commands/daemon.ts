cb460312450494768e1d4f64d49fe0671b627fcb
John Kane <john@kanej.me>
2020-12-15 16:22:37 +0000

    feat(daemon): setup reconnect and ping infrastructure

--- src/commands/daemon.ts
+++ src/commands/daemon.ts
@@ -11,6 +11,9 @@ const {
   MAILSCRIPT_DAEMON_BRIDGE_URL = 'wss://mailscript-daemon-bridge.herokuapp.com:80',
 } = process.env
 
+const PING_DELAY = 15 * 60 * 1000
+const AUTO_RECONNECT_DELAY = 1000
+
 export default class Daemon extends Command {
   static description = 'Run a daemon to execute scripts on email arrival'
 
@@ -69,26 +72,77 @@ export default class Daemon extends Command {
       withStandardErrors({}, this),
     )
 
+    this.connectWebsocket(token, flags.command)
+  }
+
+  connectWebsocket(token: string, command: string) {
     const ws = new WebSocket(MAILSCRIPT_DAEMON_BRIDGE_URL, {
       headers: {
         Authorization: `bearer ${token}`,
       },
     })
 
+    const heartbeat = (ws: WebSocket, delay: number) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      clearTimeout(ws.pingTimeout)
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      ws.pingTimeout = setTimeout(() => {
+        ws.terminate()
+      }, delay)
+    }
+
+    const ping = () => {
+      heartbeat(ws, PING_DELAY)
+    }
+
     ws.on('open', () => {
       this.log('Listenening for emails ...')
 
       ws.send(token)
+
+      ping()
     })
+
+    ws.on('ping', ping)
 
     ws.on('message', async (data: string) => {
       const { subject, text }: { subject: string; text: string } = JSON.parse(
         data,
       )
 
-      const out = await exec(flags.command, { env: { subject, text } })
+      const out = await exec(command, { env: { subject, text } })
 
       this.log(out.stdout)
     })
+
+    ws.on('close', () => {
+      ws.terminate()
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      clearTimeout(ws.pingTimeout)
+      setTimeout(() => {
+        ws.removeAllListeners()
+
+        // Start connection again
+        this.connectWebsocket(token, command)
+      }, AUTO_RECONNECT_DELAY)
+    })
+
+    ws.on('error', (err) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      if (err.code === 'ECONNREFUSED') {
+        ws.removeAllListeners()
+
+        return
+      }
+
+      ws.terminate()
+    })
+
+    return ws
   }
 }
