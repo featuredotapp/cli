df7277e5ad57858a39585b91c9b9debd42c0d3c2
John Kane <john@kanej.me>
2020-12-18 12:59:09 +0000

    feat(workflows): support adding with alias to user control addresses (#28)
    * feat(workflows): support addings workflows that alias to user controlled mailscript addresses
    
    The user controls the mailscript address if the user has an accessory for that address that
    references a key with write access.
    
    * feat(workflows): allow aliasing of email address that resolve to owned mailscript addresses
    
    For example `myuser+spam@mailscript.com`.

--- src/commands/workflows/add.ts
+++ src/commands/workflows/add.ts
@@ -7,6 +7,7 @@ import { handle } from 'oazapfts'
 import * as api from '../../api'
 import setupApiClient from '../../setupApiClient'
 import withStandardErrors from '../../utils/errorHandling'
+import resolveBaseAddress from '../../utils/resolveBaseAddress'
 import verifyEmailFlow from '../../utils/verifyEmailFlow'
 
 const workflowTypeFlags = [
@@ -455,6 +456,36 @@ export default class WorkflowsAdd extends Command {
     // User's verified email address
     if (userEmail === alias) {
       return
+    }
+
+    const {
+      list: userAccessories,
+    }: api.GetAllAccessoriesResponse = await handle(
+      client.getAllAccessories(),
+      withStandardErrors({}, this),
+    )
+
+    const baseAlias = resolveBaseAddress(alias)
+
+    const existingAccessory = userAccessories.find(
+      ({ name, type }) => type === 'mailscript-email' && name === baseAlias,
+    )
+
+    if (existingAccessory) {
+      this.debug(
+        `Checking existing mailscript address: ${existingAccessory.address}`,
+      )
+
+      if (existingAccessory.address) {
+        const { write } = await handle(
+          client.getKey(existingAccessory.address, existingAccessory.key),
+          withStandardErrors({}, this),
+        )
+
+        if (write) {
+          return
+        }
+      }
     }
 
     const { list: verifications } = await handle(
