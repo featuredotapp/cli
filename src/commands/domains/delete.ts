51be9a065667f07489e82872c4cca17437922a55
Geoffrey Casper <gcasper42@gmail.com>
2021-04-02 20:30:45 -0400

    feat(domains): Relevant commands implemented

---
+++ src/commands/domains/delete.ts
@@ -0,0 +1,60 @@
+import { Command, flags } from '@oclif/command'
+
+import * as api from '../../api'
+import setupApiClient from '../../setupApiClient'
+
+import { handle } from 'oazapfts'
+import withStandardErrors from '../../utils/errorHandling'
+
+type FlagsType = {
+  domain?: string
+
+  [key: string]: any
+}
+
+export default class DomainsDelete extends Command {
+  static description = 'delete a domain'
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    domain: flags.string({
+      char: 'd',
+      description: 'the domain to be removed from your account',
+    }),
+  }
+
+  static args = []
+
+  async run() {
+    const { flags } = this.parse(DomainsDelete)
+
+    const client = await setupApiClient()
+
+    if (!client) {
+      this.log('Could not connect to API. Check your internet connection')
+      this.exit(1)
+    }
+
+    return this.delete(client, flags)
+  }
+
+  async delete(client: typeof api, { domain }: FlagsType): Promise<void> {
+    if (!domain) {
+      this.log('Please provide a domain to delete')
+
+      this.exit(1)
+    }
+
+    return handle(
+      client.removeDomainVeriy(domain),
+      withStandardErrors(
+        {
+          '200'() {
+            this.log(domain, 'has been removed from your account')
+          },
+        },
+        this,
+      ),
+    )
+  }
+}
---
+++ src/commands/domains/list.ts
@@ -0,0 +1,52 @@
+import { Command, flags } from '@oclif/command'
+
+import * as api from '../../api'
+import setupApiClient from '../../setupApiClient'
+
+import { handle } from 'oazapfts'
+import withStandardErrors from '../../utils/errorHandling'
+
+type FlagsType = {
+  [key: string]: any
+}
+
+export default class DomainsList extends Command {
+  static description =
+    'list all domains linked wih your account (both verified and unverified)'
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+  }
+
+  static args = []
+
+  async run() {
+    this.parse(DomainsList)
+
+    const client = await setupApiClient()
+
+    if (!client) {
+      this.log('Could not connect to API. Check your internet connection')
+      this.exit(1)
+    }
+
+    return this.list(client)
+  }
+
+  async list(client: typeof api): Promise<void> {
+    return handle(
+      client.getAllDomains(),
+      withStandardErrors(
+        {
+          '200'({ id }: api.GetAllDomainsResponse) {
+            this.log('Your domains:')
+            for (const domain of id) {
+              this.log('-', domain)
+            }
+          },
+        },
+        this,
+      ),
+    )
+  }
+}
